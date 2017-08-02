bcdbf4b67abe7d90124d1995b1cfa0446519ba87
Kirk Smith <kirks@hotmail.co.uk>
2017-08-02 22:28:23 +0100

    BitmapMatrix initialized with column and row attributes

--- lib/bitmap_matrix.rb
+++ lib/bitmap_matrix.rb
@@ -1,3 +1,9 @@
 class BitmapMatrix
 
+    attr_reader :columns, :rows
+
+    def initialize(columns, rows)
+        @columns = columns
+        @rows = rows
+    end
 end
--- spec/bitmap_matrix_spec.rb
+++ spec/bitmap_matrix_spec.rb
@@ -1,3 +1,10 @@
 describe 'BitmapMatrix' do
-    subject (:matrix){BitmapMatrix.new}
+    let (:matrix){BitmapMatrix.new(5, 5)}
+
+    describe '#initialize' do
+        it 'Bitmap matrix will be 5 x 5' do
+            expect(matrix.columns).to eq 5
+            expect(matrix.rows).to eq 5
+        end
+    end
 end
